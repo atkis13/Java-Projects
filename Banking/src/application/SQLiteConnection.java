8901dbd2e427262dca878509c771d6c046020f5a
atkis13 <kis_attila_1349@yahoo.com>
2017-07-24 09:16:14 +0300

    Update SQLiteConnection.java

--- Banking/src/application/SQLiteConnection.java
+++ Banking/src/application/SQLiteConnection.java
@@ -4,7 +4,8 @@ public class SQLiteConnection {
 	public static Connection Connector(){
 		try{
 			Class.forName("org.sqlite.JDBC");
-			Connection conn = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\Aty\\Desktop\\Bank.sqlite");
+			Connection conn = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\User\\Desktop\\Bank.sqlite");
+			                                                          //database location
 			return conn;
 		}catch (Exception e){
 			System.out.println(e); 
