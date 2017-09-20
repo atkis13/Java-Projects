4ea457db70404172dce50ce1000cc131e2bbd1de
atkis13 <kis_attila_1349@yahoo.com>
2017-09-20 10:22:12 +0300

    Create Ttest.java

---
+++ RPG Equip JUnit/Ttest.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class Ttest {
+	Player mypl = new Player();
+	Sword x= new Sword();
+	
+
+	@Test
+	public void test() {
+		
+		assertEquals(7,mypl.stamina);
+		
+		
+	}
+
+}
