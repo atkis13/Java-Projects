9e3c7c5892dd80777f4733e74d71330864c4f51b
atkis13 <kis_attila_1349@yahoo.com>
2017-09-20 10:21:01 +0300

    Create Weapon.java

---
+++ RPG Equip JUnit/Weapon.java
@@ -0,0 +1,19 @@
+public class Weapon {
+	int damage;
+	public int getDamage() {
+		return damage;
+	}
+	public void setDamage(int damage) {
+		this.damage = damage;
+	}
+	public int getStamina() {
+		return stamina;
+	}
+	public void setStamina(int stamina) {
+		this.stamina = stamina;
+	}
+	int stamina;
+	
+	int magic;
+
+}
